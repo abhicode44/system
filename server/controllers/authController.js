c8c016cbf071284ed2c4f8dff02cf89c32f209ca
Abhishek Chudasama <abhishekchudasama49@gmail.com>
2024-08-21 22:28:48 +0530

    "authcontroller changes"

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -38,17 +38,7 @@ exports.loginUser = async (req, res) => {
         };
     };
 
-// Middleware to verify token
-exports.authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) return res.status(401).json({ message: 'Token required' });
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.status(403).json({ message: 'Invalid token' });
-        req.user = user; // Add user to request object
-        next(); // Pass the request to the next middleware/route handler
-    });
-};
+
 
 
